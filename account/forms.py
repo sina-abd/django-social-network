667a6ffa78ac2eadf93f13384a18b3f168874fe4
Sina <sinaedu7@gmail.com>
2024-02-21 23:54:46 +0330

    modified registration page

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,33 @@
+from typing import Any
 from django import forms
-
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError("username already exists.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError("email already exists.")
+        return email
+    
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise ValidationError("passwords do not match.")
+        print(cd.get("username"), cd.get("email"), cd.get("password1"))
--- account/views.py
+++ account/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 class RegisterView(View):
-    
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+
     def get(self, request):
-        form = UserRegistrationForm()
-        return render(request, 'account/register.html', {'form':form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password1'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form':form})
+
+
 
