5134db5d5b1e802e4747bfd40ba867610b63165b
Sina <sinaedu7@gmail.com>
2024-03-06 13:53:51 +0330

    modified:   social_network/settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sinaedu7@gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'vijnssvajvcilygo'
+EMAIL_HOST_PASSWORD = 'aneopbemwjrlefop'
 EMAIL_USE_TLS = True
 DEFULT_FROM_EMAIL = 'ONLINESHOP website'
 
